da2b8a02ee0d84f0b27819d05b821efc290bdf28
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-16 20:16:30 +0530

    added reactflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+"use client";
+import { MindMap } from "./components/MindMap";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-	);
+  return (
+    <main>
+      <MindMap />
+    </main>
+  );
 }
