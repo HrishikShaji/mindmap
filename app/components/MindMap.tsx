a6dc09bd295d043f23239ded848c9ad899641031
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-17 12:55:33 +0530

    nodes added

--- app/components/MindMap.tsx
+++ app/components/MindMap.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import ReactFlow from "reactflow";
+import ReactFlow, { Edge, Node, Position } from "reactflow";
 
 import "reactflow/dist/style.css";
 
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+const initialNodes: Node[] = [
+  {
+    id: "1",
+    type: "input",
+    position: { x: 0, y: 50 },
+    data: { label: "1" },
+    sourcePosition: Position.Right,
+  },
+  {
+    id: "2",
+    type: "output",
+    position: { x: 200, y: 0 },
+    data: { label: "2" },
+    targetPosition: Position.Left,
+  },
+  {
+    id: "3",
+    position: { x: 200, y: 100 },
+    data: { label: "3" },
+    type: "output",
+    targetPosition: Position.Left,
+  },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges: Edge[] = [
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e1-3", source: "1", target: "3" },
+];
 
 export const MindMap = () => {
   return (
