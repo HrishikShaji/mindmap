1750b417833e5814faf12c0fc26ef4832d82ec14
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-19 11:25:34 +0530

    hover added

--- app/components/Node.tsx
+++ app/components/Node.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import { Handle, Position, useNodeId } from "reactflow";
 import { useNodeEdgeContext } from "../context/NodeEdgeContext";
 import { MdDelete, MdEdit } from "react-icons/md";
@@ -14,6 +14,7 @@ function CustomNode({ data }: NodeProps) {
 	const { isEdit } = useEdit();
 	const { setNodes } = useNodeEdgeContext();
 	const { openModal } = useModal();
+	const [show, setShow] = useState(false);
 
 	if (!node) return null;
 	function deleteNode() {
@@ -21,11 +22,20 @@ function CustomNode({ data }: NodeProps) {
 	}
 
 	return (
-		<div className="w-[200px] bg-white relative flex p-1 shadow-md rounded-md  border-2 border-stone-400">
-			<div className="flex   items-center w-full justify-center">
-				<h1 className="absolute left-2">{data.id}</h1>
+		<div
+			onMouseEnter={() => setShow(true)}
+			onMouseLeave={() => setShow(false)}
+			className="w-[200px]  bg-white  flex p-1  relative shadow-md rounded-md  border-2 border-stone-400"
+		>
+			{show && (
+				<div className="bg-red-500 absolute z-10 -top-28 left-0 rounded-md h-[100px] w-[100px]">
+					{node}
+				</div>
+			)}
+			<div className="flex   items-center w-full  justify-center">
+				<h1 className=" left-2">{data.id}</h1>
 				<h1 className=" font-semibold text-sm ">{data.label}</h1>
-				{isEdit ? (
+				{isEdit && (
 					<div className="flex gap-2 absolute -top-6 p-1 rounded-t-md bg-gray-500 text-white right-2">
 						<button onClick={() => deleteNode()}>
 							<MdDelete />
@@ -34,12 +44,12 @@ function CustomNode({ data }: NodeProps) {
 							<MdEdit />
 						</button>
 					</div>
-				) : null}
+				)}
 			</div>
 			<Handle
 				type="target"
 				position={Position.Left}
-				className="w-2 h-2 !bg-teal-500"
+				className="w-2 h-2 !bg-teal-500 "
 			/>
 			<Handle
 				type="source"
