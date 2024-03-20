11a5fc448154858e4f042e7066187ea3962c294f
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-20 22:17:37 +0530

    delete nodes added

--- app/components/DeleteNode.tsx
+++ app/components/DeleteNode.tsx
@@ -2,36 +2,44 @@
 import React, { FormEvent, useState } from "react";
 import "reactflow/dist/style.css";
 import { useNodeEdgeContext } from "../context/NodeEdgeContext";
+import NodeCheckbox from "./NodeCheckBox";
 
 export const DeleteNode = () => {
-	const { setNodes } = useNodeEdgeContext();
-	const [id, setId] = useState("");
+  const { setNodes, nodes } = useNodeEdgeContext();
+  const [reset, setReset] = useState(false);
+  const [targets, setTargets] = useState<string[]>([]);
 
-	function deleteNode(e: FormEvent) {
-		e.preventDefault();
-		try {
-			setNodes((prev) => prev.filter((item) => item.id !== id));
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setId("");
-		}
-	}
+  function deleteNode(e: FormEvent) {
+    e.preventDefault();
+    try {
+      const newNodes = nodes.filter(
+        (node) => !targets.some((item) => item === node.id),
+      );
+      setNodes(newNodes);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setReset(!reset);
+    }
+  }
 
-	return (
-		<form onSubmit={deleteNode} className="flex gap-4">
-			<input
-				value={id}
-				onChange={(e) => setId(e.target.value)}
-				placeholder="id..."
-				className="p-1 rounded-md"
-			/>
-			<button
-				className="bg-white text-black py-1 px-3 rounded-md"
-				type="submit"
-			>
-				Delete
-			</button>
-		</form>
-	);
+  return (
+    <form onSubmit={deleteNode} className="flex gap-4">
+      <div className="grid grid-cols-4 gap-10">
+        {nodes.map((node, i) => (
+          <div key={i} className="flex justify-between p-1">
+            <h1>{node.data.label}</h1>
+            <h1>{node.id}</h1>
+            <NodeCheckbox id={node.id} reset={reset} onChange={setTargets} />
+          </div>
+        ))}
+      </div>
+      <button
+        className="bg-white text-black py-1 px-3 rounded-md"
+        type="submit"
+      >
+        Delete
+      </button>
+    </form>
+  );
 };
